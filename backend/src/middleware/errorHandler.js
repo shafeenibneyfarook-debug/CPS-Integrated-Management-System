e3c3106e9525d64f6be5847e48e76315e8ed4290
shafeenibneyfarook-debug <shafeen.ibney.farook@g.bracu.ac.bd>
2026-08-22 09:20:33 +0600

    Delete backend directory

--- backend/src/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-module.exports = function errorHandler(err, req, res, next) {
-  console.error(err);
-  res.status(err.statusCode || 500).json({
-    success: false,
-    message: err.message || 'Internal server error',
-    errors: err.errors || null,
-  });
-};
--- backend/src/middleware/notFound.js
+++
@@ -1,6 +0,0 @@
-module.exports = function notFound(req, res) {
-  res.status(404).json({
-    success: false,
-    message: 'Route not found',
-  });
-};
